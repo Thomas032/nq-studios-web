dcfdc727570db5bb63c70a1d17f148cda752da95
Thomas032 <80817250+Thomas032@users.noreply.github.com>
2024-10-12 10:23:15 +0300

    Adding the basic website structure - NAV and FOOTER

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header className="bg-gray-800 text-white p-4">
+      <div className="container mx-auto">
+        <h1 className="text-2xl">Empowero</h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
